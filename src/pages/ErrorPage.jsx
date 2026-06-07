8c9a15fa6b225ee84db07664ad711e356f820ae5
alfaim <hassan.alfaim@gmail.com>
2026-06-08 00:19:08 +0800

    added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,3 @@
-//react-bootstrap import(s)
-import { Container, Button } from "react-bootstrap";
-
 //library import(s)
 import { Link } from "react-router-dom";
 
@@ -11,13 +8,15 @@ const confusedPsyduckImage = "../confused-psyduck.gif";
 
 export default function ErrorPage() {
     return (
-        <Container className="error-container">
+        <div className="error-page-wrapper">
             <div className="d-flex flex-column align-items-center gap-3">
-                <h1>Uh oh! Looks like Psyduck got confused...</h1>
-                <h4>Error 404 - Page Not Found</h4>
-                <img src={confusedPsyduckImage} />
-                <Button className="button-gradient" as={Link} to="/">Go Home</Button>
+                <h1 className="error-title">Uh oh! Looks like Psyduck got confused...</h1>
+                <h4 className="error-subtitle">Error 404 - Page Not Found</h4>
+                <Link to="/">
+                    <img className="psyduck-link-home" src={confusedPsyduckImage} width="550px" />
+                </Link>
+                <p className="psyduck-hint">Click on Psyduck to return home!</p>
             </div>
-        </Container>
+        </div>
     );
 }
